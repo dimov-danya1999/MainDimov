3eb0e23ac3486af978381d53c91141bfee8f566a
dimov-danya1999 <100965240+dimov-danya1999@users.noreply.github.com>
2022-11-04 16:17:49 -0700

    Initial Commit

---
+++ MainProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainProject
+//
+//  Created by Даниил Димов on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
